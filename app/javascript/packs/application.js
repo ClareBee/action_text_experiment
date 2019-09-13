f105c03d16a889288a6511fbf2ea345ae2e182a9
Clare <chblackburne@gmail.com>
2019-09-13 21:51:32 +0100

    styles and loading js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "bootstrap"
+require("trix")
+require("@rails/actiontext")
+import "controllers"
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
@@ -14,11 +18,3 @@ require("channels")
 //
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
-
-require("trix")
-require("@rails/actiontext")
-import "bootstrap";
-document.addEventListener
-import "../styles/index.scss";
-
-import "controllers"
