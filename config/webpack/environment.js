ffa6b42701c10f77d4bcb92c76148631aff07154
Clare <chblackburne@gmail.com>
2019-09-13 21:51:14 +0100

    add bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,4 @@
 
 const { environment } = require("@rails/webpacker");
-const webpack = require("webpack");
-
-// https://webpack.js.org/plugins/provide-plugin/
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
-
 
 module.exports = environment
