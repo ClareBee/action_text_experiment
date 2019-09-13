de7e5f6cecd57559395a2db8a4a82d6ac1fe9c0c
Clare <chblackburne@gmail.com>
2019-09-13 21:50:51 +0100

    endpoints

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
+    @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -10,12 +13,11 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new
+      render :new, notice: 'Failed to create'
     end
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
@@ -25,11 +27,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
+    if @article.destroy
+      redirect_to articles_path, notice: 'successfully deleted'
+    else
+      redirect_to articles_path, notice: 'failed to delete'
+    end
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :text)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
